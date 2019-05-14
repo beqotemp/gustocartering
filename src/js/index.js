63dc51490a8a7a6b8d8c47efc5791bc0ae54923e
beqotemp <godunovofc@gmail.com>
2019-05-14 04:27:48 +0300

    nav and some fixes

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import "./scripts/menu.js";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 import "./scripts/sliders.js";
+import "./scripts/second-slider.js";
 import "./scripts/screen.js";
 
 
